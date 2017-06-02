73db4746c5126e2948a0d3481b6b3055aa215c63
Frederik Nordahl Jul Sabroe <frederikns@gmail.com>
2017-06-02 15:43:19 +0200

    Added test infrastructure.

---
+++ src/test/kotlin/com/blackwoodseven/kubernetes/volume_backup/MainTest.kt
@@ -0,0 +1,14 @@
+package com.blackwoodseven.kubernetes.volume_backup
+
+import kotlin.test.assertEquals
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+
+class MainTest : Spek({
+    describe("the system should be sane") {
+        it("should calculate correctly") {
+            assertEquals(2 + 2, 4)
+        }
+    }
+})
