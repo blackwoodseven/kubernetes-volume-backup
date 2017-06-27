c1ee128a6ac3a400dbc068d412f4653db9d1ef11
Frederik Nordahl Jul Sabroe <frederikns@gmail.com>
2017-06-27 15:21:11 +0200

    Added handling of EnvirontmentVariable config.

--- src/main/kotlin/com/blackwoodseven/kubernetes/volume_backup/Main.kt
+++ src/main/kotlin/com/blackwoodseven/kubernetes/volume_backup/Main.kt
@@ -1,5 +1,30 @@
 package com.blackwoodseven.kubernetes.volume_backup
 
+import com.natpryce.konfig.*
+import com.natpryce.konfig.ConfigurationProperties.Companion.systemProperties
+
+object backup : PropertyGroup() {
+    val directories by stringType
+    val cron by stringType
+    val pod_name by stringType
+    val pod_namespace by stringType
+    val container_name by stringType
+}
+
+object aws : PropertyGroup() {
+    val default_region by stringType
+    val bucket by stringType
+    val access_key_id by stringType
+    val secret_access_key by stringType
+}
+
+
+
 fun main(args : Array<String>) {
-    println("Hello, world!")
+    val config = systemProperties() overriding
+                 EnvironmentVariables()
+
+    println(config[backup.directories])
+    println(config[aws.default_region])
+
 }
